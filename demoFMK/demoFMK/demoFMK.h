daffdd52bfead9ca4b9bc578b3385e88f76762b6
Asmita Borawake <asmita.borawake@innoplexus.com>
2018-02-20 12:49:02 +0530

    add data

---
+++ demoFMK/demoFMK/demoFMK.h
@@ -0,0 +1,19 @@
+//
+//  demoFMK.h
+//  demoFMK
+//
+//  Created by asmita.borawake on 20/02/18.
+//  Copyright © 2018 asmita.borawake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for demoFMK.
+FOUNDATION_EXPORT double demoFMKVersionNumber;
+
+//! Project version string for demoFMK.
+FOUNDATION_EXPORT const unsigned char demoFMKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoFMK/PublicHeader.h>
+
+
---
+++ demoFMK/service.swift
@@ -0,0 +1,18 @@
+//
+//  service.swift
+//  demoFMK
+//
+//  Created by asmita.borawake on 20/02/18.
+//  Copyright © 2018 asmita.borawake. All rights reserved.
+//
+
+import Foundation
+
+
+public class service {
+    private init(){}
+    
+    public static func stringdata() -> String{
+        return "demo of framework using private repo"
+    }
+}
